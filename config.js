f47775339ffda5ee81c03a66959e1daf2bfb6122
Hrafnir <eirik.gaarde@gmail.com>
2025-07-22 17:09:30 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+/* Version: #297 */
+// === SENTRAL KONFIGURASJON ===
+// Denne filen inneholder alle nøkler og konstanter for MQuiz-prosjektet.
+// Den skal lastes FØR alle andre script-filer i HTML.
+
+// --- SUPABASE ---
+const SUPABASE_URL = 'https://ldmkhaeauldafjzaxozp.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkbWtoYWVhdWxkYWZqemF4b3pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwNjY0MTgsImV4cCI6MjA2ODY0MjQxOH0.78PkucLIkoclk6Wd6Lvcml0SPPEmUDpEQ1Ou7MPOPLM';
+
+// --- SPOTIFY ---
+const SPOTIFY_CLIENT_ID = '61939bcc94514b76bcdc268a7b258740';
+const SPOTIFY_REDIRECT_URI = 'https://hrafnir.github.io/Musikk-Quiz/index.html';
+const SPOTIFY_SCOPES = [
+    'streaming', 'user-read-email', 'user-read-private', 'user-modify-playback-state'
+];
+
+console.log('config.js lastet.');
+/* Version: #297 */
